93809ebe83fa0fb41f72dded7daa2536d8c03a7c
Tao Huang <taohms@gmail.com>
2018-06-22 14:12:14 +0800

    Add 20130815 version

--- lib/azure/sas/string_to_sign.rb
+++ lib/azure/sas/string_to_sign.rb
@@ -15,7 +15,29 @@ module Azure
               (@options.signedstart && @options.signedstart.utc.iso8601).to_s,
               (@options.signedexpiry && @options.signedexpiry.utc.iso8601).to_s,
               @canonicalized_resource.to_s,
-              @options.identifier.to_s
+              @options.identifier.to_s,
+              '2012-02-12'
+            ].compact.join("\n").force_encoding('UTF-8')
+          end
+        end
+      end
+
+      module V20130815
+        # @see https://docs.microsoft.com/en-us/rest/api/storageservices/fileservices/constructing-a-service-sas
+        class Blob
+          def initialize(canonicalized_resource, options)
+            @canonicalized_resource = canonicalized_resource
+            @options = options
+          end
+
+          def generate
+            [
+              @options.signedpermissions.to_s,
+              (@options.signedstart && @options.signedstart.utc.iso8601).to_s,
+              (@options.signedexpiry && @options.signedexpiry.utc.iso8601).to_s,
+              @canonicalized_resource.to_s,
+              @options.identifier.to_s,
+              '2013-08-15'
             ].compact.join("\n").force_encoding('UTF-8')
           end
         end
